f3ea513a2e3f08e4fe8df7aed7256ab475a21ebe
min <7kyeongmin@gmail.com>
2018-12-07 14:20:56 +0900

    TicTacToe ShowGameOver

--- game.js
+++ game.js
@@ -9,18 +9,22 @@ module.exports = function(server){
     var rooms = [];
   
     io.on('connect', function(socket){ 
-        console.log('Connection: ' + socket.id);
+        console.log('Connected: ' + socket.id);
+
 
         if (rooms.length > 0){  
             var rID = rooms.shift();
             socket.join(rID, function(){
                 console.log("JoinRoom: " + rID);
                 socket.emit('joinRoom', {room: rID});
+                //자신 제외 특정방에  ?    socket.broadcast.to(room).emit('exitRoom'); 
+                //자신 포함 특정방에  ?    io.to(rID).emit('startGame');
+                io.to(rID).emit('startGame');
             });
         }else{
             var roomName = uuidv4();
-            socket.join(roomName, function(){
-                console.log("CreateRoom: " + roomName);
+            socket.join(roomName, function() {
+                console.log('CreateRoom: ' + roomName);
                 socket.emit('createRoom', {room: roomName});
                 rooms.push(roomName); 
             });
@@ -28,11 +32,23 @@ module.exports = function(server){
 
 
 
-
-
-        socket.on('disconnect', function(reson ){
+        socket.on('disconnecting', function(reson ){
             console.log('Disconnected: '+socket.id);
+            var socketRooms = Object.keys(socket.rooms).filter( item=> item != socket.id );
+            console.dir(socketRooms);
+
+            socketRooms.forEach(function(room){
+                socket.broadcast.to(room).emit('exitRoom');  
+
+                //혼자 있는 방의 유저가 disconnect 되면 해당 방 제거
+                var idx = rooms.indexOf(room);
+                if(idx != -1){
+                    rooms.splice(idx, 1);
+                }
+            });
+            
         });
+
 
         socket.on('hi',function() {
             console.log('Hi~~');
@@ -43,9 +59,17 @@ module.exports = function(server){
 
         socket.on('message', function(msg){
             console.dir(msg);
-            socket.broadcast.emit('chat',msg);
+            socket.broadcast.emit('chat', msg);
         });
 
+
+        socket.on('doPlayer', function(playerInfo){
+            //방이름을 전달하도록 //socket.broadcast.to(room).emit('exitRoom'); 
+            var roomID = playerInfo.room;
+            var cellIndex = playerInfo.position;
+            socket.broadcast.to(roomID).emit('doOpponent', 
+            {position : cellIndex});
+        });
 
     });
 };
